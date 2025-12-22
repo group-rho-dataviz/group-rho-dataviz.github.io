c4e6fe12a544d113f9c9dec4aa8be79fea5b5fde
fabriziosardo <fabry.sardo@gmail.com>
2025-12-22 17:50:47 +0100

    Add initial HTML structure and styling for conflict data visualization project Integrated Tailwind CSS for styling and layout.

---
+++ src/js/tailwind_config.js
@@ -0,0 +1,19 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                'brand-blue': '#002677',
+                'brand-yellow': '#F4DA40',
+                'brand-red': '#C8102E',
+                'brand-cyan': '#199BFC',
+                'brand-black': '#25282A',
+                'brand-gray': '#D9D9D6',
+                'dibris': '#007C58',                        
+            },
+            fontFamily: {
+                'heading': ['Roboto Slab', 'serif'],
+                'body': ['Fira Sans', 'sans-serif'],
+            }
+        }
+    }
+}
